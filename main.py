b2b6f6d2b76025fbf6c12c7af35c871b151a9fd7
roatt-tilma <roshanbhattmnr@gmail.com>
2020-09-08 00:01:11 +0545

    added venv to .gitignore

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 from Classes.player import Player, Game
 from Classes.Information import PlayerInfo, Question, Admin
+from tkinter import *
+
+
+# creating window
+root = Tk()
+width, height = root.winfo_width(), root.winfo_height()
+root.geometry("%dx%d+0+0" %(800, 800))
+root.title("Who's the Boss? - Grouped Game Challenge")
 
 admin_handle = Admin()
 player_handle = PlayerInfo()
 game_handle = Game()
 
-print("*******************MAIN MENU*****************")
-print("1. Start Quiz")
-print("2. Add questions")
-
-choice = int(input("Enter choice: "))
-
-if choice == 1:
+def start_game():
     name = player_handle.get_name()
     regno = player_handle.get_regno()
     player1 = Player(name, regno)  
@@ -20,7 +22,18 @@ if choice == 1:
     game_handle.start_game(question_stacks, 1, 1, player1, 5)
     print(player1.name, ", your score is: ", player1.score)
 
-elif choice == 2:
+# creating label
+#label = Label(root, text = "First Label", bg = "black", fg = "blue")
+#label.grid(row = 0, column = 0)
+
+# creating button
+myButton = Button(root, text = "Start Game", command = start_game, bg = 'red', fg = 'green')
+myButton.pack()
+
+# running mainloops
+root.mainloop()
+
+def add_questions():
     print("Choose difficulty level")
     print("1. Easy\n2. Medium\n3. Hard\n4. HOT")
     diff_lvl = int(input())
